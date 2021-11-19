3464ee5bd1e4e79abc9f3b8b5c67c99215b72c05
David Clark <david.w.clark@hotmail.co.uk>
2021-11-19 10:55:33 +0000

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-const indexRouter = require('./routes/index')
 
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({ limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -25,5 +27,6 @@ db.once('open', () => console.log("Connected to Mongoose"))
 
 
 app.use ('/', indexRouter)
+app.use ('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
